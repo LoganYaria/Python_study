c6b34bcdf72759153ee69408245f3de0868d6f49
KUTS <aglushevichyaroslavgr.2307@gmail.com>
2024-02-06 02:27:03 +0300

    Extracting some PDF pages

--- PDF/extraction/pdf_extraction_to_pdf.py
+++ PDF/extraction/pdf_extraction_to_pdf.py
@@ -1,9 +1,19 @@
-from PyPDF2 import PdfFileWriter
+from PyPDF2 import PdfFileWriter, PdfFileReader
 from pathlib import Path
 
+# #Создание пустого поля в новом pdf файле
 pdf_path = Path.cwd()/'New_PDF_file.pdf'
-
 pdf_file = PdfFileWriter()#присваиваем объекту класс записи pdf
 pdf_file.addBlankPage(width=72, height=72)#добавляем пустую область в объект рзамером 1 кв дюйм
 with open(pdf_path, mode='wb') as new_pdf_file:#открываем файл с модом wb запись битов
     pdf_file.write(new_pdf_file) # метод .write  используется именно пакетом PyPDF2, !НЕ! pathlib
+
+# #Извечение первой страницы
+pdf_path_out = Path.cwd()/'Pride_and_Prejudice.pdf'
+pdf_path_in = Path.cwd()/'New_PDF_1.pdf'
+pdf_file_out = PdfFileReader(str(pdf_path_out))
+page_0 = pdf_file_out.getPage(0)# получение 0 страницы из PDF файла
+pdf_file_in = PdfFileWriter()
+pdf_file_in.addPage(page_0)# добавления 0 страницы в объект PdfFileWriter
+with pdf_path_in.open(mode='wb') as new_file:
+    pdf_file_in.write(new_file)# запись файла
--- PDF/extraction/pdf_extraction_to_txt.py
+++ PDF/extraction/pdf_extraction_to_txt.py
@@ -1,10 +1,15 @@
+# #Извлечение pdf файла в txt файл
 from pathlib import Path
 from PyPDF2 import PdfFileReader
-
+# #Обозначение путей
 pdf_path = Path.cwd()/'Pride_and_Prejudice.pdf'
 txt_path = Path.cwd()/'PrIdE_AnD_PrEjUdIcE.txt'
-#txt_path.touch()
+# #Работа с пакетом PyPDF2
 pdf = PdfFileReader(str(pdf_path))
+title = pdf.documentInfo.title
+num_pages = pdf.getNumPages()
+# #Работа с извлечением
 with txt_path.open(mode='a',encoding='utf-8',newline='') as txt_file:
+    txt_file.write(f'{title}\nNumber of pages: {num_pages}\n\n')
     for page in pdf.pages:
         txt_file.writelines(page.extractText())
